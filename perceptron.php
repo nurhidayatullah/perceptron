b3c4b7445fe2a2823f25e2fb974eaf1f46f658ec
Nur Hidayatullah <nur_hidayat_45@yahoo.com>
2015-04-29 10:10:38 +0700

    Update perceptron.php

--- perceptron.php
+++ perceptron.php
@@ -5,38 +5,44 @@ class Perceptron{
 	private $weight;
 	private $bias;
 	
-	function set_learning_rate($learning_rate){
+	function set_learning_rate($learning_rate){		// function untuk mengeset learning rate
 		$this->learning_rate = $learning_rate;
 	}
 	
-	function set_weight($weight){
+	function set_weight($weight){					// function untuk mengeset bobot awal
 		$this->weight = $weight;
 	}
 	
-	function set_bias($bias){
+	function set_bias($bias){ 						// function untuk mengeset bias awal
 		$this->bias = $bias;
 	}
 	
-	function training($data,$label,$max_epoh){      //function pelatihan
-		for($a = 0;$a < $max_epoh;$a++){
+	function training($data,$label,$max_epoh){      // function pelatihan
+		for($a = 0;$a < $max_epoh;$a++){			// perulangan sebanyak epoh
 			$cek ="";
-			for($x = 0; $x < count($data); $x++) {
+			echo "Epoh Ke : ".($a+1)."</br>";
+			for($x = 0; $x < count($data); $x++) {	// perulangan untuk mengolah data per baris
 				$weight = $this->weight;
 				$bias = $this->bias;
-				$y = $this->dot_product($data[$x],$weight,$bias);
-				if($y == $label[$x]){
+				$y = $this->dot_product($data[$x],$weight,$bias);	// melakukan perhitungan nilai y
+				if($y == $label[$x]){			// jika output sama dengan label, bobot dan bias tidak berubah
 					$cek .="1";
 					$this->weight = $weight;
 					$this->bias = $bias;
-				}else{
+				}else{							// jika output berbeda, hitung error dan update bobot beserta bias
 					$cek .="0";
 					$error = $label[$x] - $y;
 					$this->weight = $this->update_weight($weight,$this->learning_rate,$error,$data[$x]);
 					$this->bias = $this->update_bias($bias,$this->learning_rate,$error);
 				}
+				echo "Bobot : ";
+				for($b=0;$b<count($this->weight);$b++){
+					echo $this->weight[$b]."   ";
+				}
+				echo "Bias : ".$this->bias."</br>";
 			}
-			$ck = strpos($cek,"0");
-			if($ck===FALSE){
+			$ck = strpos($cek,"0");					// cek apakah dalam satu iterasi ada error
+			if($ck===FALSE){						// jika tidak ada error, iterasi dihentikan
 				$a = $max_epoh;
 			}
 		}
@@ -44,7 +50,7 @@ class Perceptron{
 		return $out;
 	}
 	
-	function update_weight($weight,$learning_rate,$error,$data){
+	function update_weight($weight,$learning_rate,$error,$data){	// function update bobot 
 		$weight_new = array();
 		for($x = 0;$x < count($weight);$x++){
 			$weight_new[$x] = $weight[$x] +($learning_rate*$error*$data[$x]);
@@ -52,21 +58,21 @@ class Perceptron{
 		return $weight_new;
 	}
 	
-	function update_bias($bias,$learning_rate,$error){
+	function update_bias($bias,$learning_rate,$error){				// function update bobot bias
 		$bias_new = $bias +($learning_rate*$error);
 		return $bias_new;
 	}
 	
-	function dot_product($data,$weight,$bias){
+	function dot_product($data,$weight,$bias){	// menghitung nilai y_in,
 		$y_in = 0;
 		for($x = 0;$x < count($data);$x++){
 			$y_in = $y_in + ($data[$x]*$weight[$x]);
 		}
 		$y = $y_in + ($bias);
-		return $this->sign($y);
+		return $this->sign($y);					// mengaktivasi y_in
 	}	
 	
-	function sign($y_in){
+	function sign($y_in){						// fungsi aktivasi undak biner
 		if($y_in>=0){
 			$y = 1;
 		}else{
@@ -97,6 +103,7 @@ $nn = new Perceptron();
 $nn->set_learning_rate($learning_rate);
 $nn->set_weight($weight);
 $nn->set_bias($bias);
+echo "proses pelatihan :</br>";
 $out = $nn->training($data,$label,$max_epoh);	//melakukan training untuk mendapatkan bobot dan bias 
 
 $bobot = $out[0];
